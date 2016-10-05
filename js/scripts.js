00102403b540bf4c836949109c6fb594b4dc96d4
Jenkins <jenkins@sifirbir.org>
2016-10-05 03:49:25 +0000

    new post java-override-anotasyonu

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-$(window).resize(function() {
-    if( footerHeight !== $('footer').height()) {
-        footerHeight = $('footer').height()
-        configureFooter()
-    }
-})
+// $(window).resize(function() {
+//     if( footerHeight !== $('footer').height()) {
+//         footerHeight = $('footer').height();
+//         configureFooter()
+//     }
+// });
 
 function configureFooter() {
     $('#main').css({
         'padding-bottom' : footerHeight + "px"
-    })
+    });
 
     $('footer').css({
         'margin-top' : '-' + footerHeight + "px"
@@ -16,8 +16,8 @@ function configureFooter() {
 }
 
 $(document).ready(function () {
-    footerHeight = $('footer').height()
-    configureFooter()
+    // footerHeight = $('footer').height();
+    // configureFooter();
     $("#menu-button").click(function () {
         $("#mobile-menu").sidebar('toggle')
     })
